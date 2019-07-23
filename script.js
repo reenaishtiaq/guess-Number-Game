911d722254fd88eeaebfaf4d4bd63e15e1d8ebb6
reenaishtiaq <49521474+reenaishtiaq@users.noreply.github.com>
2019-07-23 16:14:56 +0500

    WIP

--- script.js
+++ script.js
@@ -1,23 +1,73 @@
 
+// for easy level
+var num = Math.floor(Math.random() * 10);
 
 function myNumber() {
-    var num = Math.floor(Math.random() * 10 + 1);
-    let myInput = document.querySelector("input").value;
+    let myInput = parseInt(document.querySelector("#userInput").value);
 
 
 
-    if (num < myInput) {
-        document.querySelector("p").innerHTML = "Too Small";
+    if (num > myInput) {
+        document.querySelector("p").innerHTML = " Too small";
 
 
     } else {
-        if (num > myInput) {
+        if (num < myInput) {
             document.querySelector("p").innerHTML = "Too large";
 
-        }else  {
+        }else if(num == myInput) {
             document.querySelector("p").innerHTML = "Waaoo!! you got";
 
 
         }
     }
 }
+
+// // // for medium level
+
+
+// function myNumber() {
+//     var num = Math.floor(Math.random() * 100);
+//     let myInput = document.querySelector("input").value;
+
+
+
+//     if (num < myInput) {
+//         document.querySelector("p").innerHTML = "Too Small";
+
+
+//     } else {
+//         if (num > myInput) {
+//             document.querySelector("p").innerHTML = "Too large";
+
+//         }else  {
+//             document.querySelector("p").innerHTML = "Waaoo!! you got";
+
+
+//         }
+//     }
+// }
+
+// // // for hard level
+
+// function myNumber() {
+//     var num = Math.floor(Math.random() * 400);
+//     let myInput = document.querySelector("input").value;
+
+
+
+//     if (num < myInput) {
+//         document.querySelector("p").innerHTML = "Too Small";
+
+
+//     } else {
+//         if (num > myInput) {
+//             document.querySelector("p").innerHTML = "Too large";
+
+//         }else  {
+//             document.querySelector("p").innerHTML = "Waaoo!! you got";
+
+
+//         }
+//     }
+// }
