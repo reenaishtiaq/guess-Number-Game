e7f6bceb588bff6150226c57e52fdc80fcaf8199
reenaishtiaq <49521474+reenaishtiaq@users.noreply.github.com>
2019-07-28 03:26:20 +0500

    Added a file

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 
-let numEasy = parseInt(Math.random() * 10);
-let numMedium = parseInt(Math.random() * 100);
-let numHard = parseInt(Math.random() * 500);
 
 document.querySelector("#easy").style = "display: none;";
 document.querySelector("#medium").style = "display: none;";
 document.querySelector("#hard").style = "display: none;";
 document.querySelector("#user").style = "display: none;";
 
+let num;
+let attempt = 0;
+
 // for easy level page 
 function easy() {
+    num = parseInt(Math.random() * 10);
+    
+    document.body.style.backgroundImage = "url('images/easy2.jpg')";
     document.querySelector("#easy").style = "display: flex;";
     document.querySelector("#medium").style = "display: none;";
     document.querySelector("#hard").style = "display: none;";
@@ -18,6 +21,9 @@ function easy() {
 
 // for medium level page
 function medium() {
+    num = parseInt(Math.random() * 100);
+    
+    document.body.style.backgroundImage = "url('images/medium1.jpg')";
     document.querySelector("#easy").style = "display: none;";
     document.querySelector("#medium").style = "display: flex;";
     document.querySelector("#hard").style = "display: none;";
@@ -26,63 +32,33 @@ function medium() {
 
 // for hard level page
 function hard() {
+    num = parseInt(Math.random() * 500);
+    
+    document.body.style.backgroundImage = "url('images/hard2.jpg')";
     document.querySelector("#easy").style = "display: none;";
     document.querySelector("#medium").style = "display: none;";
     document.querySelector("#hard").style = "display: flex;";
     document.querySelector("#user").style = "display: flex;";
 }
 
-// Easy level
-
 function displayResult() {
-
+    attempt++
+    
     let myInput = document.querySelector(".userinput").value;
 
-    if (myInput == "") {
-        alert("please enter a number")
-    } else {
-        if (myInput < numEasy) {
-            alert("too small");
-        } else if (myInput > numEasy) {
-            alert("too large");
-        } else if (myInput == numEasy) {
-            alert("congratulate");
+    if (attempt < 10) {
+        if (myInput == "") {
+            alert("please enter a number")
+        } else {
+            if (myInput < num) {
+                alert("Too low! Guess again!");
+            } else if (myInput > num) {
+                alert("Too high! Guess again!");
+            } else if (myInput == num) {
+                alert("Congratulations!! You got me in " + attempt + " attempts." + " I was thinking " + num); 
+            }
         }
-    }
-}
-// medium level
-function displayResult() {
-    let myInput = document.querySelector(".userinput").value;
-
-    if (myInput == "") {
-        alert("please enter a number")
     } else {
-        if (numMedium > myInput) {
-            alert("too small");
-        } else if (numMedium < myInput) {
-            alert("too large");
-        } else if (numMedium == myInput) {
-            alert("congratulate");
-
-        }
-
-    }
-}
-// hard levl
-function displayResult() {
-    let myInput = document.querySelector(".userinput").value;
-
-    if (myInput == "") {
-        alert("please enter a number")
-    } else {
-        if (numHard > myInput) {
-            alert("too small");
-        } else if (numHard < myInput) {
-            alert("too large");
-        } else if (numHard == myInput) {
-            alert("congratulate");
-
-        }
-
+        alert("You Lost! Please try again")
     }
 }
